21534745828f81ec121c48a7fafaeacdaaa38c60
Eleeey <ezralaakasen@gmail.com>
2024-04-10 17:51:51 +0100

    ....

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,28 +1,35 @@
 import Collection from '@/components/shared/Collecton';
 import { Button } from '@/components/ui/button';
 import { getEventsByUser } from '@/lib/actions/events.action';
+import { getOrdersByUser } from '@/lib/actions/order.action';
+import { IOrder } from '@/lib/database/models/order.model';
+import { SearchParamProps } from '@/types';
 import { auth } from '@clerk/nextjs';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react'
 
-const profilePage = async () => {
+const profilePage = async ({ searchParams }: SearchParamProps) => {
   const { sessionClaims } = auth();
   const userId = sessionClaims?.userId as string;
   const userImage = sessionClaims?.userImage as string;
   const userFirstName = sessionClaims?.userFirstName as string;
-  console.log(userImage, userFirstName)   
+  console.log(userImage, userFirstName);
+  const ordersPage = Number(searchParams?.ordersPage) || 1;
+  const eventsPage = Number(searchParams?.eventsPage) || 1;
 
-  const eventsByUser = await getEventsByUser({userId, page:1})
+  const orders = await getOrdersByUser({ userId, page: ordersPage });
+  const orderedEvents = orders?.data.map((order: IOrder) => order.event) || [];
+
+  const eventsByUser = await getEventsByUser({ userId, page: ordersPage });
   return (
     <>
-      <section className="flex flex-row w-full">
-        
+      {/* <section className="flex flex-row w-full">
         <div>
           <p>HEY</p>
           <h3>{userFirstName}</h3>
         </div>
-      </section>
+      </section> */}
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
         <div className="wrapper flex items-center justify-center sm:justify-between">
           <h3 className="h3-bold text-center sm:text-left">My Tickets</h3>
@@ -33,16 +40,16 @@ const profilePage = async () => {
       </section>
 
       <section className="wrapper my-8">
-        {/* <Collection
-          data={}
+        <Collection
+          data={orderedEvents}
           emptyTitle="No event tickets purchased yet"
           emptyStateSubtext="No worries - plenty of exciting events to explore!"
           collectionType="My_Tickets"
           limit={3}
           page={1}
           urlParamName="ordersPage"
-          totalPages={}
-        /> */}
+          totalPages={orders?.totalPages}
+        />
       </section>
 
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
@@ -65,9 +72,9 @@ const profilePage = async () => {
           urlParamName="eventsPage"
           totalPages={2}
         />
-      </section> 
+      </section>
     </>
   );
-}
+};
 
 export default profilePage
